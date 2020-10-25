2fbd0241a74abe704ebd5a8369971f44492366e0
jarnemeekers1999 <jarne.meekers@hotmail.com>
2020-10-25 17:01:38 +0100

    Changed directory name

---
+++ Opdracht1 _Cluster/serverscript.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#consul configuration
+sed -i '/#server = true/c\server = true' /etc/consul.d/consul.hcl
+
+lastLine=`tail -1 /etc/consul.d/consul.hcl`
+if [ $lastLine!='bind_addr = "192.168.1.10"' ]
+then
+cat <<EOF >>/etc/consul.d/consul.hcl
+bind_addr = "192.168.1.10"
+EOF
+fi
+
+sudo systemctl start consul
+
+sudo mkdir /opt/nomad/server
+
+#nomad configuration
+cat <<EOF >/etc/nomad.d/nomad.hcl
+data_dir = "/opt/nomad/server"
+bind_addr = "{{ GetInterfaceIP \"eth1\" }}"
+server {
+    enabled = true
+    bootstrap_expect = 1
+}
+EOF
+
+#start nomad and enable consul and nomad
+sudo systemctl start nomad
+sudo systemctl enable consul
+sudo systemctl enable nomad
+
+
+
